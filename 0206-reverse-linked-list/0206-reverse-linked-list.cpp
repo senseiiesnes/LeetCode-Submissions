c854732d20b7397b104cfabb5e790e7922435782
Kushal Jha <97540649+senseiiesnes@users.noreply.github.com>
2023-12-31 07:40:02 -0800

    Time: 11 ms (6.22%), Space: 8.8 MB (27.53%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,15 +10,31 @@
  */
 class Solution {
 public:
+    
+    ListNode* recursiveReverse(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newHead = recursiveReverse(head->next);
+        ListNode* front = head->next;
+        head->next = NULL;
+        front->next = head;
+        return newHead;
+    }
+    
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-        while(temp != NULL){
-            ListNode* front = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = front;
-        }
-        return prev;
+        
+//                 Iterative Approach
+        
+        // ListNode* temp = head;
+        // ListNode* prev = NULL;
+        // while(temp != NULL){
+        //     ListNode* front = temp->next;
+        //     temp->next = prev;
+        //     prev = temp;
+        //     temp = front;
+        // }
+        // return prev;
+        
+//                 Recursive Approach
+        return recursiveReverse(head);
     }
 };
